8c4135c5d56026eee9742c87bb7cd4e12e4bb6a0
dasunwnl <137673136+dasunwnl@users.noreply.github.com>
2023-07-01 22:38:37 +0530

    Update s3.config.php

--- Containers/nextcloud/config/s3.config.php
+++ Containers/nextcloud/config/s3.config.php
@@ -1,27 +1,16 @@
 <?php
-if (getenv('OBJECTSTORE_S3_BUCKET')) {
-  $use_ssl = getenv('OBJECTSTORE_S3_SSL');
-  $use_path = getenv('OBJECTSTORE_S3_USEPATH_STYLE');
-  $use_legacyauth = getenv('OBJECTSTORE_S3_LEGACYAUTH');
-  $autocreate = getenv('OBJECTSTORE_S3_AUTOCREATE');
-  $CONFIG = array(
-    'objectstore' => array(
-      'class' => '\OC\Files\ObjectStore\S3',
-      'arguments' => array(
-        'bucket' => getenv('OBJECTSTORE_S3_BUCKET'),
-        'key' => getenv('OBJECTSTORE_S3_KEY') ?: '',
-        'secret' => getenv('OBJECTSTORE_S3_SECRET') ?: '',
-        'region' => getenv('OBJECTSTORE_S3_REGION') ?: '',
-        'hostname' => getenv('OBJECTSTORE_S3_HOST') ?: '',
-        'port' => getenv('OBJECTSTORE_S3_PORT') ?: '',
-        'objectPrefix' => getenv("OBJECTSTORE_S3_OBJECT_PREFIX") ? getenv("OBJECTSTORE_S3_OBJECT_PREFIX") : "urn:oid:",
-        'autocreate' => (strtolower($autocreate) === 'false' || $autocreate == false) ? false : true,
-        'use_ssl' => (strtolower($use_ssl) === 'false' || $use_ssl == false) ? false : true,
-        // required for some non Amazon S3 implementations
-        'use_path_style' => $use_path == true && strtolower($use_path) !== 'false',
-        // required for older protocol versions
-        'legacy_auth' => $use_legacyauth == true && strtolower($use_legacyauth) !== 'false'
-      )
-    )
-  );
-} 
+$CONFIG = array (
+'objectstore' => array (
+        'class' => '\\OC\\Files\\ObjectStore\\S3',
+        'arguments' => array (
+                'bucket' => 'nextcloud',
+                'region' => 'ap-mumbai-1',
+                'hostname' => 'bmkltsly13vb.compat.objectstorage.ap-mumbai-1.oraclecloud.com',
+                'key' => '9b816df9287bfb97f4ae7f00faa8fbe890d07992',
+                'secret' => 'u7j5q0r/EQ4YG/xUR0MxvycJ1rGtj2nSKTQC0FD8leA=',
+                'port' => 443,
+                // required for some non-Amazon S3 implementations
+                'use_path_style' => true,
+        ),
+),
+);
